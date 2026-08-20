29e5ea2cc544ee245ee308b3189c3f373f321cc8
communityguard <134632556+communityguard@users.noreply.github.com>
2026-08-20 15:25:08 -0400

    Add notification module

---
+++ app-04.js
@@ -0,0 +1,6 @@
+/* ---------- toast ---------- */
+let toastT = null;
+function toast(msg) {
+  const t = $('#toast'); t.textContent = msg; t.classList.add('show');
+  clearTimeout(toastT); toastT = setTimeout(() => t.classList.remove('show'), 2200);
+}
